4bec48ce64eafc3a19dce9e9c86bf121efb8fb46
Yihui Xie <xie@yihui.name>
2009-06-14 07:52:14 +0000

    give a warning message and try to download 'tagcloud.swf' if 'tagcloud.swf' is not under the same path with 'tagcloud.xml'

--- R/tagCloud.R
+++ R/tagCloud.R
@@ -5,10 +5,13 @@ function(tagData, htmlOutput = "tagCloud.html",
     hicolor = "ff0000", distr = "true", tspeed = 100, version = 9, 
     bgcolor = "ffffff", useXML = FALSE, htmlTitle = "Tag Cloud", 
     noFlashJS, target = NULL, scriptOnly = FALSE, encode = FALSE) {
-    if (missing(SWFPath)) 
-        SWFPath = "http://www.roytanck.com/wp-content/plugins/wp-cumulus/tagcloud.swf"
+    missingSWF = missing(SWFPath)
+    if (missingSWF) 
+        SWFPath = "http://yihui.name/en/wp-content/uploads/2009/06/tagcloud.swf"
+    if (missingSWF & useXML) 
+        SWFPath = "tagcloud.swf"
     if (missing(JSPath)) 
-        JSPath = "http://www.roytanck.com/wp-content/plugins/wp-cumulus/swfobject.js"
+        JSPath = "http://yihui.name/en/wp-content/uploads/2009/06/swfobject.js"
     if (missing(noFlashJS)) 
         noFlashJS = "This will be shown to users with no Flash or Javascript."
     tagXML = sprintf("<tags>%s</tags>", paste(sprintf("<a href='%s' style='%s'%s%s%s>%s</a>", 
@@ -23,15 +26,25 @@ function(tagData, htmlOutput = "tagCloud.html",
             tagData$hicolor, ""), ""), tagData$tag), collapse = ""))
     if (encode)
         tagXML = URLencode(tagXML)
-    if (useXML) 
-        cat(tagXML, file = file.path(dirname(htmlOutput), "tagCloud.xml"))
+    if (useXML) {
+        cat(tagXML, file = file.path(dirname(htmlOutput), "tagcloud.xml"))
+        if (!("tagcloud.swf" %in% list.files(dirname(htmlOutput)))) {
+            warning("The Flash file 'tagcloud.swf' must be under the same path with your XML file 'tagcloud.xml'!", 
+                immediate. = TRUE)
+            SWFurl = "http://yihui.name/en/wp-content/uploads/2009/06/tagcloud.swf"
+            if (tolower(substr(readline("Do you want to download the Flash file automatically? (y/n) "), 1, 1) == "y")) {
+                download.file(SWFurl, file.path(dirname(htmlOutput), "tagcloud.swf"), mode = "wb")
+            }
+        }
+    }
     cat(ifelse(scriptOnly, "", sprintf("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n<title>%s</title>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n</head>\n\n<body>", 
         htmlTitle)), sprintf("\t<script type=\"text/javascript\" src=\"%s\"></script>", 
         JSPath), sprintf("\t<div id=\"%s\">%s</div>", divId, 
-        noFlashJS), sprintf("\t<script type=\"text/javascript\">\n\t\tvar so = new SWFObject(\"%s\", \"tagcloud\", \"%d\", \"%d\", \"%d\", \"#%s\");\n%s\t\tso.addVariable(\"mode\", \"tags\");\n\t\tso.addVariable(\"tcolor\", \"0x%s\");\n\t\tso.addVariable(\"tcolor2\", \"0x%s\");\n\t\tso.addVariable(\"hicolor\", \"0x%s\");\n\t\tso.addVariable(\"tspeed\", \"%d\");\n\t\tso.addVariable(\"distr\", \"%s\");\n%s\n\t\tso.write(\"%s\");\n\t\t</script>\n", 
+        noFlashJS), sprintf("\t<script type=\"text/javascript\">\n\t\tvar so = new SWFObject(\"%s\", \"tagcloud\", \"%d\", \"%d\", \"%d\", \"#%s\");\n%s%s\t\tso.addVariable(\"tcolor\", \"0x%s\");\n\t\tso.addVariable(\"tcolor2\", \"0x%s\");\n\t\tso.addVariable(\"hicolor\", \"0x%s\");\n\t\tso.addVariable(\"tspeed\", \"%d\");\n\t\tso.addVariable(\"distr\", \"%s\");\n%s\n\t\tso.write(\"%s\");\n\t\t</script>\n", 
         SWFPath, width, height, version, bgcolor, ifelse(transparent, 
             "\t\tso.addParam(\"wmode\", \"transparent\");\n", 
-            ""), tcolor, tcolor2, hicolor, tspeed, distr, ifelse(useXML, 
+            ""), ifelse(useXML, "", "\t\tso.addVariable(\"mode\", \"tags\");\n"), 
+            tcolor, tcolor2, hicolor, tspeed, distr, ifelse(useXML, 
             "\t\tso.addVariable(\"xmlpath\", \"tagcloud.xml\");", 
             sprintf("\t\tso.addVariable(\"tagcloud\", \"%s\");", 
                 tagXML)), divId), ifelse(scriptOnly, "", "</body>\n\n</html>"), 
@@ -39,4 +52,3 @@ function(tagData, htmlOutput = "tagCloud.html",
         if (!scriptOnly)
             cat("HTML file created at ", htmlOutput, "\n")
 }
-
