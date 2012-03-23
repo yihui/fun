f642222ff0546caecbb71b29ecea6322375f971d
Yihui Xie <xie@yihui.name>
2012-03-23 00:06:42 -0500

    just copy tagcloud.swf and swfobject.js from this package

--- R/tag_cloud.R
+++ R/tag_cloud.R
@@ -60,20 +60,13 @@
 ##'     if (!interactive()) browseURL(htmlFile)
 ##' }
 tag_cloud = function(tagData, htmlOutput = "tagCloud.html",
-    SWFPath, JSPath, divId = "tagCloudId", width = 600, height = 400,
+    SWFPath = 'tagcloud.swf', JSPath = 'swfobject.js', divId = "tagCloudId", width = 600, height = 400,
     transparent = FALSE, tcolor = "333333", tcolor2 = "009900",
     hicolor = "ff0000", distr = "true", tspeed = 100, version = 9,
     bgcolor = "ffffff", useXML = FALSE, htmlTitle = "Tag Cloud",
     noFlashJS, target = NULL, scriptOnly = FALSE, encode = FALSE,
     reserved = FALSE) {
     tagData$tag = htmlspecialchars(tagData$tag)
-    missingSWF = missing(SWFPath)
-    if (missingSWF)
-        SWFPath = "http://yihui.name/en/wp-content/uploads/2009/06/tagcloud.swf"
-    if (missingSWF & useXML)
-        SWFPath = "tagcloud.swf"
-    if (missing(JSPath))
-        JSPath = "http://yihui.name/en/wp-content/uploads/2009/06/swfobject.js"
     if (missing(noFlashJS))
         noFlashJS = "This will be shown to users with no Flash or Javascript."
     tagXML = sprintf("<tags>%s</tags>", paste(sprintf("<a href='%s' style='%s'%s%s%s>%s</a>",
@@ -90,15 +83,9 @@ tag_cloud = function(tagData, htmlOutput = "tagCloud.html",
         tagXML = URLencode(tagXML, reserved)
     if (useXML) {
         cat(tagXML, file = file.path(dirname(htmlOutput), "tagcloud.xml"))
-        if (!("tagcloud.swf" %in% list.files(dirname(htmlOutput)))) {
-            warning("The Flash file 'tagcloud.swf' must be under the same path with your XML file 'tagcloud.xml'!",
-                immediate. = TRUE)
-            SWFurl = "http://yihui.name/en/wp-content/uploads/2009/06/tagcloud.swf"
-            if (tolower(substr(readline("Do you want to download the Flash file automatically? (y/n) "), 1, 1) == "y")) {
-                download.file(SWFurl, file.path(dirname(htmlOutput), "tagcloud.swf"), mode = "wb")
-            }
-        }
     }
+    file.copy(system.file("js", c("tagcloud.swf", "swfobject.js"), package = "fun"), 
+              dirname(htmlOutput))
     cat(ifelse(scriptOnly, "", sprintf("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n<title>%s</title>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n</head>\n\n<body>",
         htmlTitle)), sprintf("\t<script type=\"text/javascript\" src=\"%s\"></script>",
         JSPath), sprintf("\t<div id=\"%s\">%s</div>", divId,
