eb7953d6317c5f93ec7e0af93497319c8b75dd94
Tal Galili <tal.galili@gmail.com>
2013-03-05 23:15:40 +0200

    adding wait = FALSE to shell

--- R/shutdown.R
+++ R/shutdown.R
@@ -21,6 +21,6 @@
 ##' }
 shutdown = function(wait = 0) {
     Sys.sleep(wait)
-    ifelse(.Platform$OS.type == "windows", shell("shutdown -s -t 0"),
+    ifelse(.Platform$OS.type == "windows", shell("shutdown -s -t 0", wait = FALSE),
         system("shutdown -h now"))
 }
